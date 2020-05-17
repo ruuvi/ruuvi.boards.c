60cbecd0b527553c2f9c23ee52a0fce8a44b875c
Otso Jousimaa <otso@ojousima.net>
2020-05-17 15:05:58 +0300

    Support Ruuvi GW, PCA boards

--- README.md
+++ README.md
@@ -13,6 +13,9 @@ Open an issue or pull request at GitHub.
 BSD-3
 
 # Changelog
+## 0.5.0 
+ - Add support for PCA10040, PCA10056E, RuuviGW nRF
+
 ## 0.4.0
  - Convert active states to arrays on Kaarle, Kalervo
 
--- ruuvi_boards.h
+++ ruuvi_boards.h
@@ -10,29 +10,38 @@
 #ifndef RUUVI_BOARDS_H
 #define RUUVI_BOARDS_H
 
-#define RUUVI_BOARDS_SEMVER "0.3.5"
+#define RUUVI_BOARDS_SEMVER "0.5.0"
 /** @brief Port+pin definition for GPIO pin. */
 #define RB_PORT_PIN_MAP(PORT, PIN) ((PORT<<8) + (PIN))
 /** @brief Pin not in use on this board. */
 #define RB_PIN_UNUSED              (0xFFFF)
 
 #ifdef BOARD_PROPRIETARY
-  #include "proprietary_board.h"
+#   include "proprietary_board.h"
 #endif
 #ifdef BOARD_RAUNO
-  #include "ruuvi_board_rauno.h"
+#   include "ruuvi_board_rauno.h"
 #endif
 #ifdef BOARD_RUUVITAG_B
-  #include "ruuvi_board_ruuvitag_b.h"
+#   include "ruuvi_board_ruuvitag_b.h"
 #endif
 #ifdef BOARD_KAARLE
-  #include "ruuvi_board_kaarle.h"
+#   include "ruuvi_board_kaarle.h"
 #endif
 #ifdef BOARD_KALERVO
-  #include "ruuvi_board_kalervo.h"
+#   include "ruuvi_board_kalervo.h"
 #endif
 #ifdef BOARD_KEIJO
-  #include "ruuvi_board_keijo.h"
+#   include "ruuvi_board_keijo.h"
+#endif
+#ifdef BOARD_PCA10040
+#   include "ruuvi_board_pca10040.h"
+#endif
+#ifdef BOARD_PCA10056E
+#   include "ruuvi_board_pca10056e.h"
+#endif
+#ifdef BOARD_RUUVIGW_NRF
+#   include "ruuvi_board_gwnrf.h"
 #endif
 
 #endif
