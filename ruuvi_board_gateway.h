8a62bbf4163a050990499222e56fe0a69c5ffa80
Alexey Skvortsov <alexeyn.skvortsov@gmail.com>
2020-09-30 15:15:08 +0700

    ruuvi_board_gateway.h: RB_UART_ESP2NRF: change from RB_GWBUS_2 to RB_GWBUS_1 for compatibility between rev.A1 and rev.A2

--- ruuvi_board_gateway.h
+++ ruuvi_board_gateway.h
@@ -11,9 +11,8 @@
 
 #include "ruuvi_boards.h"
 
-#define RB_NRF_STATUS_PIN       RB_GWBUS_1    //!< nRF selftest, reset pin.
 #define RB_UART_NRF2ESP         RB_GWBUS_3    //!< UART NRF -> ESP
-#define RB_UART_ESP2NRF         RB_GWBUS_2    //!< UART ESP -> NRF
+#define RB_UART_ESP2NRF         RB_GWBUS_1    //!< UART ESP -> NRF
 #define RB_UART_ESP_RTS         RB_PIN_UNUSED 
 #define RB_UART_ESP_CTS         RB_PIN_UNUSED
 #define RB_HWFC_ENABLED         (0U)
