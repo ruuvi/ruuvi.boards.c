c3d812ccd0f376c7c5521c0626c97723633f067f
Otso Jousimaa <otso@ojousima.net>
2026-01-29 13:08:01 +0800

    Add STHS34PF80 thermal IR presence sensor board definitions (#24)

--- ruuvi_board_pca10040.h
+++ ruuvi_board_pca10040.h
@@ -86,6 +86,8 @@
 #define RB_LED_ACTIVITY              RB_LED_1
 #define RB_LED_STATUS_OK             RB_LED_1
 #define RB_LED_STATUS_ERROR          RB_LED_2
+#define RB_LED_BUTTON_PRESS          RB_LED_1
+#define RB_LED_CONFIG_ENABLED        RB_LED_2
 
 // Button definitions for PCA10040
 #define RB_BUTTON_PWR_PIN_NUMBER     0
@@ -112,12 +114,15 @@
 #define RB_SPI_FREQ                  RB_SPI_FREQUENCY_8M
 
 // I2C definitions for PCA10040
-#define RB_I2C_SDA_PIN               RB_PORT_PIN_MAP(0, 4)
-#define RB_I2C_SCL_PIN               RB_PORT_PIN_MAP(0, 5)
+// Using P0.26 (SDA) and P0.27 (SCL) to avoid conflict with UART/interface MCU
+#define RB_I2C_SDA_PIN               RB_PORT_PIN_MAP(0, 26)
+#define RB_I2C_SCL_PIN               RB_PORT_PIN_MAP(0, 27)
 #define RB_I2C_FREQUENCY_100k        0
 #define RB_I2C_FREQUENCY_250k        1
 #define RB_I2C_FREQUENCY_400k        2
 #define RB_I2C_FREQ                  RB_I2C_FREQUENCY_100k
+#define RB_I2C_MAX_SPD               RB_I2C_FREQUENCY_400k
+#define RB_I2C_BUS_POWER_PIN         RB_PIN_UNUSED
 
 // UART definitions for PCA10040
 #define RB_UART_TX_PIN               RB_PORT_PIN_MAP(0, 12)
@@ -171,4 +176,12 @@
 #define RB_NTC_ACTIVE                     1                       // NTC active state
 #define RB_NTC_PWR_PIN                    RB_PORT_PIN_MAP(0, 6)   //!< NTC power pin
 #define RB_NTC_ADC                        RB_AIN1                 //!< NTC ADC
+
+// STHS34PF80 Infrared Presence Sensor (connect externally to I2C pins)
+#define RB_ENVIRONMENTAL_STHS34PF80_PRESENT  (1U)  //!< Enable for testing with external sensor
+#define RB_STHS34PF80_I2C_ADDRESS            (0x5AU) //!< Fixed 7-bit I2C address
+#define RB_STHS34PF80_I2C_MAX_SPD            RB_I2C_FREQUENCY_400k
+#define RB_STHS34PF80_SENSOR_POWER_PIN       RB_PIN_UNUSED  //!< External power
+#define RB_INT_STHS34PF80_PIN                RB_PIN_UNUSED  //!< Optional interrupt
+
 #endif
